211dd8255c6cc7cb9c57cf36153ae5c9f61d4953
Arsene Tochemey GANDOTE - [SMSGH] <arsene@smsgh.com>
2014-07-30 12:02:08 +0000

    Commit

---
+++ BasicHttpClient/RestClient/HttpWebRequestAsyncState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicRestClient.RestClient
+{
+    public class HttpWebRequestAsyncState
+    {
+        public byte[] RequestBytes { get; set; }
+        public HttpWebRequest HttpWebRequest { set; get; }
+        public Exception Exception { set; get; }
+        public object State { set; get; }
+    }
+}
---
+++ BasicHttpClient/RestClient/HttpWebResponseAsyncState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicRestClient.RestClient
+{
+    public class HttpWebResponseAsyncState
+    {
+        public WebResponse WebResponse { set; get; }
+        public Exception Exception { set; get; }
+        public object State { set; get; }
+        public HttpWebRequest HttpWebRequest { set; get; }
+    }
+}
